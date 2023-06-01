a349e6e0402112767fb834ad697c9f340ad32e22
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-06-01 14:17:03 +0100

    fixed mongo restart issue with save()

--- iwtsc/server/server.js
+++ iwtsc/server/server.js
@@ -9,13 +9,15 @@ mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${process.env.DB_MONGO_HOST}/${process.env.MONGO_DB}`);
 
 mongoose.connection.on('open', function () {
-  console.log('Connected to mongo server.');
+console.log('Connected to mongo server.');
+
+
 })
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -25,7 +27,7 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
-  extended: true
+extended: true
 }));
 
 // database - Postgres
@@ -37,34 +39,35 @@ const Role = db.role;
 const Questions = require('./app/mongoModels/questions.model')
 
 async function init() {
-  await sleep(5000);
+await sleep(5000);
 }
 
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+return new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
 }
 
 
 init().then(() => {
-  // db.sequelize.sync();
-  // force: true will drop the table if it already exists
-  db.sequelize.sync({
-    force: true
-  }).then(() => {
-    console.log('Drop and resync DB');
-    initial();
-  });
+// db.sequelize.sync();
+// force: true will drop the table if it already exists
+mongoose.connection.dropCollection('questions'); //drop questions collection and its data 
+db.sequelize.sync({
+  force: true
+}).then(() => {
+  console.log('Drop and resync DB');
+  initial();
+});
 })
 
 
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the app"
-  });
+res.json({
+  message: "Welcome to the app"
+});
 });
 
 // routes
@@ -76,131 +79,132 @@ require('./app/routes/stars.routes')(app)
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+console.log(`Server is running on port ${PORT}.`);
 });
 
 function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
+Role.create({
+  id: 1,
+  name: "user"
+});
 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
+Role.create({
+  id: 2,
+  name: "moderator"
+});
 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
+Role.create({
+  id: 3,
+  name: "admin"
+});
 
-  const question1 = new Questions({
-    // id: 1,  //dont need id
-    qtype: "q",
-    urltitle: "bin_search",
-    fulltitle: "Binary Search",
-    qtext: "This is some question text about binary search, isn't it?",
-    metadata: JSON.stringify({
-      'answers': [{
-        'text': 'Yes',
-        'correct': true
-      }, {
-        'text': 'No',
-        'correct': false
-      }, {
-        'text': 'Maybe',
-        'correct': false
-      }, {
-        'text': 'All of the above',
-        'correct': false
-      }]
-    })
+
+const question1 = new Questions({
+  // id: 1,  //dont need id
+  qtype: "q",
+  urltitle: "bin_search",
+  fulltitle: "Binary Search",
+  qtext: "This is some question text about binary search, isn't it?",
+  metadata: JSON.stringify({
+    'answers': [{
+      'text': 'Yes',
+      'correct': true
+    }, {
+      'text': 'No',
+      'correct': false
+    }, {
+      'text': 'Maybe',
+      'correct': false
+    }, {
+      'text': 'All of the above',
+      'correct': false
+    }]
   })
+})
 
-  const question2 = new Questions({
-    //   id: 2, //dont need id
-    qtype: "q",
-    urltitle: "avl_trees",
-    fulltitle: "AVL Trees",
-    qtext: "This is some question text about AVL Trees, isn't it?",
-    metadata: JSON.stringify({
-      'answers': [{
-        'text': 'Yes',
-        'correct': true
-      }, {
-        'text': 'No',
-        'correct': false
-      }, {
-        'text': 'Maybe',
-        'correct': false
-      }, {
-        'text': 'All of the above',
-        'correct': false
-      }]
-    })
+const question2 = new Questions({
+  //   id: 2, //dont need id
+  qtype: "q",
+  urltitle: "avl_trees",
+  fulltitle: "AVL Trees",
+  qtext: "This is some question text about AVL Trees, isn't it?",
+  metadata: JSON.stringify({
+    'answers': [{
+      'text': 'Yes',
+      'correct': true
+    }, {
+      'text': 'No',
+      'correct': false
+    }, {
+      'text': 'Maybe',
+      'correct': false
+    }, {
+      'text': 'All of the above',
+      'correct': false
+    }]
   })
+})
 
-  const question3 = new Questions({
-    //   id: 3, //dont need id
-    qtype: "q",
-    urltitle: "java_types",
-    fulltitle: "Java Types",
-    qtext: "This is some question text about Java types, isn't it?",
-    metadata: JSON.stringify({
-      'answers': [{
-        'text': 'Yes',
-        'correct': true
-      }, {
-        'text': 'No',
-        'correct': false
-      }, {
-        'text': 'Maybe',
-        'correct': false
-      }, {
-        'text': 'All of the above',
-        'correct': false
-      }]
-    })
+const question3 = new Questions({
+  //   id: 3, //dont need id
+  qtype: "q",
+  urltitle: "java_types",
+  fulltitle: "Java Types",
+  qtext: "This is some question text about Java types, isn't it?",
+  metadata: JSON.stringify({
+    'answers': [{
+      'text': 'Yes',
+      'correct': true
+    }, {
+      'text': 'No',
+      'correct': false
+    }, {
+      'text': 'Maybe',
+      'correct': false
+    }, {
+      'text': 'All of the above',
+      'correct': false
+    }]
   })
+})
 
-  //RP: insert these questions into mongo
-  question1.save().then(() => {
-    console.log('question 1 added successfully.');
-  })
-  question2.save().then(() => {
-    console.log('question 2 added successfully.');
-  })
-  question3.save().then(() => {
-    console.log('question 3 added successfully.');
-  })
+//RP: insert these questions into mongo
+question1.save().then(() => {
+  console.log('question 1 added successfully.');
+})
+question2.save().then(() => {
+  console.log('question 2 added successfully.');
+})
+question3.save().then(() => {
+  console.log('question 3 added successfully.');
+})
 
 
-  // Questions.create({
-  //     id: 1, 
-  //     qtype: "q",
-  //     urltitle: "bin_search",
-  //     fulltitle: "Binary Search",
-  //     qtext: "This is some question text about binary search, isn't it?",
-  //     metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
-  // })
+// Questions.create({
+//     id: 1, 
+//     qtype: "q",
+//     urltitle: "bin_search",
+//     fulltitle: "Binary Search",
+//     qtext: "This is some question text about binary search, isn't it?",
+//     metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
+// })
 
-  // Questions.create({
-  //   id: 2, 
-  //   qtype: "q",
-  //   urltitle: "avl_trees",
-  //   fulltitle: "AVL Trees",
-  //   qtext: "This is some question text about AVL Trees, isn't it?",
-  //   metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
-  // })
+// Questions.create({
+//   id: 2, 
+//   qtype: "q",
+//   urltitle: "avl_trees",
+//   fulltitle: "AVL Trees",
+//   qtext: "This is some question text about AVL Trees, isn't it?",
+//   metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
+// })
 
-  // Questions.create({
-  //   id: 3, 
-  //   qtype: "q",
-  //   urltitle: "java_types",
-  //   fulltitle: "Java Types",
-  //   qtext: "This is some question text about Java types, isn't it?",
-  //   metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
-  // })
+// Questions.create({
+//   id: 3, 
+//   qtype: "q",
+//   urltitle: "java_types",
+//   fulltitle: "Java Types",
+//   qtext: "This is some question text about Java types, isn't it?",
+//   metadata: JSON.stringify({ 'answers': [{ 'text': 'Yes', 'correct': true }, { 'text': 'No', 'correct': false }, { 'text': 'Maybe', 'correct': false }, { 'text': 'All of the above', 'correct': false }] })
+// })
 
 }
